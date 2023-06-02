4710905bc3c510294e750a9ca39b68fa4e08b6fb
Taylor.R <nguyenhieucoder47@gmail.com>
2023-06-02 17:03:26 +0700

    Init dev

---
+++ src/taylordevs/CmdScheduler/Loader.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace taylordevs\CmdScheduler;
+
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase {
+
+    protected function onEnable(): void
+    {
+    }
+}
